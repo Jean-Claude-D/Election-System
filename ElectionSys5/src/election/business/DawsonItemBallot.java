a402e9731c8b0fd17d985499707d3e5023f76aae
m.hos <mariahossain6@gmail.com>
2017-09-29 16:24:22 -0400

    Override the Equals() method

--- ElectionSys5/src/election/business/DawsonItemBallot.java
+++ ElectionSys5/src/election/business/DawsonItemBallot.java
@@ -1,18 +1,23 @@
 package election.business;
 
+import lib.email;
+
 public class DawsonItemBallot implements BallotItem {
 	
 	
 	private String choiceString;
 	private int value;
 	private final int maxValue;
+	private static final long serialVersionUID = 42031768871L;
 	
 	
 	public DawsonItemBallot(String choiceString, int maxValue){
 		
+		validateBallotItem();
 		this.choiceString=choiceString;
 		this.maxValue=maxValue;
 		this.value=0;
+		
 		
 	}
 	
@@ -24,7 +29,40 @@ public class DawsonItemBallot implements BallotItem {
 		
 	}
 	
-	public 
+	public void validateBallotItem() {
+		
+		if (choiceString == null && maxValue <1 ) {
+			
+			throw new IllegalArgumentException("not valid");
+		}
+		
+	}
+	
+@Override
+	
+	public boolean equals(Object DawsonItemBallot){
+
+		  if (this instanceof DawsonItemBallot) {
+			  
+		  
+		    if (DawsonItemBallot.toString().equalsIgnoreCase(this.getChoice())) {
+		      
+		      return true;
+		    }
+		  
+		  }
+		return false;  
+	}	
+	
+	public void setValue(int value) {
+		
+		if (!(0 <= value && value <= maxValue)) {
+			
+			throw new IllegalArgumentException("not valid");
+		}
+		
+		this.value=value;
+	}
 	
 
 }
