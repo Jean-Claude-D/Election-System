98fe87553dc92a24e9c7d437b51c60b2b6f34e45
Gor_F <felicia.gorgatchov@gmail.com>
2017-09-30 01:40:28 -0700

    Add more to PostalCodeTest class

--- Common5/src/lib/PostalCodeTest.java
+++ Common5/src/lib/PostalCodeTest.java
@@ -2,77 +2,78 @@ package lib;
 
 public class PostalCodeTest {
 	
-	public static void compareToTest() {
+	public static void compareToTest(PostalCode code1, PostalCode code2, PostalCode code3) {
+		System.out.println("TEST COMPARETO()--------------------------------------------------------------------------------------------------------------------");
+		System.out.println("\tComparing code \"1\" with code \"2\" should return 0");
+		System.out.println("\tComparing code \"1\" with code \"3\" should return 1");
+		System.out.println("\tComparing code \"3\" with code \"2\" should return -1");
+	}
+	
+	public static void equalsTest(PostalCode code1, PostalCode code2, PostalCode code3) {
+		System.out.println("\n\nTEST EQUALS()--------------------------------------------------------------------------------------------------------------------");
+		System.out.println("\tComparing code \"1\" with code \"2\" should return true");
+		
+		if(code1.equals(code2)) {
+			System.out.println("\t\tcode1:" + code1.getCode() + " is equal to code2:" + code2.getCode());
+			System.out.println("\t\tTEST PASSED");
+		}else {
+			System.out.println("\t\tTEST FAILED");
+		}
+		
+		System.out.println("\tComparing code \"1\" with code \"3\" should return false");
+		
+		if(code1.equals(code3)) {
+			System.out.println("\t\tTEST FAILED");
+		}else {
+			System.out.println("\t\tcode1:" + code1.getCode() + " is not equal to code3:" + code2.getCode());
+			System.out.println("\t\tTEST PASSED");
+		}
+	}
+//*********************************************************
+	public static void hashCodeTest(PostalCode code) {
+		System.out.println("\n\nTEST HASHCODE()--------------------------------------------------------------------------------------------------------------------");
 		
 	}
-	
-	public static void equalsTest() {
+
+	public static void getCodeTest(PostalCode code) {
+		System.out.println("\n\nTEST GETCODE()--------------------------------------------------------------------------------------------------------------------");
+		System.out.println("\tPassing code \"j4x 2g3\" should get \"J4X2G3\" ");
 		
+		if(code.getCode().equals("J4X2G3")) {
+			System.out.println("\t\t" + code.getCode());
+			System.out.println("\t\tTEST PASSED");
+		}else {
+			System.out.println("\t\tTEST FAILED");
+		}
+	}
+//*********************************************************
+	public static void inRangeTest(PostalCode code) {
+		System.out.println("\n\nTEST INRANGE()--------------------------------------------------------------------------------------------------------------------");
 	}
 
-	public static void hashCodeTest() {
-	
-	}
-
-	public static void getCodeTest() {
-	
-	}
-
-	public static void inRangeTest() {
-	
-	}
-
-	public static void toStringTest() {
+	public static void toStringTest(PostalCode code) {
+		System.out.println("\n\nTEST TOSTRING()--------------------------------------------------------------------------------------------------------------------");
+		System.out.println("\tPassing code \"\" should return in all uppercase");
+		
+			if (code.getCode().equals("")) {
+				System.out.println("\t\t" + code.getCode());
+				System.out.println("\t\tTEST PASSED");
+			}else {
+				System.out.println("\t\tTEST FAILED");
+			}
 		
 	}
 	
 	public static void validateTest() {
+		System.out.println("\n\nTEST VALIDATE()--------------------------------------------------------------------------------------------------------------------");
+		System.out.println("\tPassing \"j4x 2g3\" ");
+		System.out.println("\tIt should print the code in all caps without a white space in the middle");
 		
-	}
-	
-	
-	public static void main (String [] args) {
-		
-		PostalCode goodCode = new PostalCode ("J4X 2G3");
-		PostalCode goodLowerCode = new PostalCode ("j4x 2g3");
-		
-//test compareTo()
-		
-//test equals()
-		
-//test hashCode()
-		
-//test getCode()
-		
-//test inRange()	
 		try {
-			System.out.println("\n\nTEST INRANGE()--------------------------------------------------------------------------------------------------------------------");
-			System.out.println("\tPassing \"j4x 2g3\" ");
-			System.out.println("\tIt should print the code in all caps without a white space in the middle");
-			if(goodLowerCode.getCode().equals("J4X2G3")) {
+			PostalCode validLowerCode = new PostalCode ("j4x 2g3");
+			if(validLowerCode.getCode().equals("J4X2G3")) {
 				System.out.println("\tTEST PASSED:");
-				System.out.println("\t\t" + goodLowerCode.getCode());
-			} 
-			else {
-				System.out.println("\tTEST FAILED:" + goodLowerCode.getCode());
-			}
-		}catch (IllegalArgumentException e){
-				System.out.println("\tTEST FAILED:" + e);
-			}
-		
-//test toString()
-		
-//test validate()
-		try {
-			System.out.println("\n\nTEST VALIDATE()--------------------------------------------------------------------------------------------------------------------");
-			System.out.println("\tPassing \"j4x 2g3\" ");
-			System.out.println("\tIt should print the code in all caps without a white space in the middle");
-			if(goodLowerCode.getCode().equals("J4X2G3")) {
-				System.out.println("\tTEST PASSED:");
-				System.out.println("\t\t" + goodLowerCode.getCode());
-			} 
-			else {
-				System.out.println("\tTEST FAILED:" + goodLowerCode.getCode());
+				System.out.println("\t\t" + validLowerCode.getCode());
 			}
 		}catch (IllegalArgumentException e){
 				System.out.println("\tTEST FAILED:" + e);
@@ -81,7 +82,7 @@ public class PostalCodeTest {
 		try {
 			System.out.println("\n\tPassing \"4J2 X3G\" ");
 			System.out.println("\tIt should not work because it is not a valid postal code");
-			PostalCode invalidCode = new PostalCode ("4J2 X3G");
+			PostalCode code2 = new PostalCode ("4J2 X3G");
 		}catch (IllegalArgumentException e){
 			System.out.println("\tTEST PASSED: " + e);
 		}
@@ -89,19 +90,28 @@ public class PostalCodeTest {
 		try {
 			System.out.println("\n\tPassing \"123\" ");
 			System.out.println("\tIt should not work because it is not a valid postal code");
-			PostalCode shortCode = new PostalCode ("123");
+			PostalCode code3 = new PostalCode ("123");
 		}catch (IllegalArgumentException e){
 			System.out.println("\tTEST PASSED: " + e);
 		}
 		
 		try {
-			System.out.println("\n\tPassing \"abc\" ");
+			System.out.println("\n\tPassing \"a1b2c3d4e5\" ");
 			System.out.println("\tIt should not work because it is not a valid postal code");
-			PostalCode longCode = new PostalCode ("a1b2c3d4e5");
+			PostalCode code4 = new PostalCode ("a1b2c3d4e5");
 		}catch (IllegalArgumentException e){
 			System.out.println("\tTEST PASSED: " + e);
 		}
+	}
+	
+	
+	public static void main (String [] args) {
 		
+		PostalCode goodCode = new PostalCode ("J4X 2G3");
+		PostalCode goodLowerCode = new PostalCode ("j4x 2g3");
+		
+
+
 
 		
 	
