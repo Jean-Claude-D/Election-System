fb4ffeabb2925027d730ef9efdde8007b1eb59d5
Cao Hoang, Nguyen <nch1942@gmail.com>
2017-10-02 15:44:18 -0400

    Tecnically finish....technically...

--- ElectionSys5/src/election/business/DawsonElection.java
+++ ElectionSys5/src/election/business/DawsonElection.java
@@ -1,35 +1,32 @@
-/**
- * 
- */
+
 package election.business;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
-
+import java.util.Objects;
+import election.business.interfaces.Ballot;
+import election.business.interfaces.BallotItem;
 import election.business.interfaces.Election;
 import election.business.interfaces.Tally;
+import election.business.interfaces.Voter;
 
 /**
- * @author Crusader2142
- *
+ * Name Class create an Object that will represent a voter for the Election System Project.
+ * @author Cao Hoang Nguyen
+ * @version 1.5
+ * @since 20th September, 2017
  */
 public class DawsonElection implements Election {
 	private static final long serialVersionUID = 42031768871L;
 	private String name;
 	private String type;
 	private ElectionType electType;
-	private int startYear;
-	private int startMonth;
-	private int startDay;
 	private LocalDate startDate;
-	private int endYear;
-	private int endMonth;
-	private int endDay;
 	private LocalDate endDate;
 	private String startRange;
 	private String endRange;
 	private Tally tally;
-	private String[] items;
+	private BallotItem[] ballotItems;
 	
 	public DawsonElection(String name, String type, int startYear, int
 			startMonth, int startDay, int endYear, int endMonth, int endDay,
@@ -45,9 +42,6 @@ public class DawsonElection implements Election {
 		LocalDate endDate = LocalDate.of (endYear, endMonth, endDay);
 		
 		validStartEnd(startDate, endDate);
-		
-		
-		
 		checkTally(tally);
 		
 		this.name = name;
@@ -55,12 +49,10 @@ public class DawsonElection implements Election {
 		this.startRange = startRange;
 		this.endRange = endRange;
 		this.tally = tally;
-		this.items = items;
+		this.ballotItems = checkItem(items);
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.electType = electionTypeCheck(type);
-
-		
 	}
 	
 
@@ -68,10 +60,38 @@ public class DawsonElection implements Election {
 		return startDate;
 	}
 	
+	public String[] getElectionChoices() {
+		String[] choices = new String[this.ballotItems.length];
+		
+		for (int i = 0; i < this.ballotItems.length; i++)
+		{
+			choices[i] = this.ballotItems[i].toString();
+		}
+		return choices;
+	}
+	
 	public LocalDate getEndDate() {
 		return endDate;
 	}
 	
+	public StubBallot getBallot() {
+		BallotItem[] temp = new BallotItem[this.ballotItems.length];
+		
+		for (int i = 0; i < this.ballotItems.length; i++)
+		{
+			temp[i] = this.ballotItems[i];
+		}
+		StubBallot stubBallot = new StubBallot(this.ballotItems, this);
+		return stubBallot;
+	}
+	
+	public Ballot getBallot(Voter v) {
+		if(v.isEligible(this)) {
+			return (Ballot) this.getBallot();
+		}
+		throw new IllegalArgumentException ("Voter is not eligible");
+	}
+
 	public boolean isLimitedToPostalRange() {
 		if ( (this.getPostalRangeStart() == null) || (this.getPostalRangeEnd() == null))
 		{
@@ -80,9 +100,15 @@ public class DawsonElection implements Election {
 		return true;
 	}
 	
-	public StubBallot getBallot() {
-		return //    THIS IS NOT FINISH   <=======================================================
+	public void castBallot(Ballot b, Voter v) {
+		if (!v.isEligible(this))
+		{
+			throw new UnsupportedOperationException ("VOTER IS NOT ELIGIBLE");
+		}
+		b.validateSelections();
+		this.tally.update(b);
 	}
+	
 	
 	public void setTally(Tally tally) {
 		checkTally(tally);
@@ -117,35 +143,13 @@ public class DawsonElection implements Election {
 	}
 	
 	public int getTotalVotesCast() {
-		throw new UnsupportedOperationException ("SHIT HAPPENED");
+		throw new UnsupportedOperationException ("THIS METHOD IS NOT SUPPORTED");
 	}
 	
 	public int getInvalidVoteAttempts() {
-		throw new UnsupportedOperationException ("SHIT HAPPENED");
+		throw new UnsupportedOperationException ("THIS METHOD IS NOT SUPPORTED");
 	}
-	
-	@Override
-	public String toString() {
-	    String election = (this.name + "*" + startDate.getYear() + "*" + startDate.getMonthValue()
-	    + "*" + startDate.getDayOfMonth() + "*" + endDate.getYear() + "*" + endDate.getMonthValue()
-	    + "*" + endDate.getDayOfMonth() + "*" + this.startRange + "*" + this.endRange
-	    + "*" + this.type );  //    THIS IS NOT FINISH   <=======================================================
-	    return election;
-	}
-	
-	@Override
-	public int compareTo(Election name) {
-		if (name == null)
-		{
-			throw new IllegalArgumentException("Object Name is Null");
-		}
 		
-		int check = this.getName().compareToIgnoreCase(name.getName());
-		return check;
-	}
-	
-	
-	
 	private void nullChecker (String check) {
 		if ( (check == null) || (check.isEmpty()) )
 		{
@@ -170,10 +174,7 @@ public class DawsonElection implements Election {
 	
 	private ElectionType electionTypeCheck (String type) {
 		type = type.toUpperCase().trim();
-//		ElectionType elecType[] = ElectionType.values();
-//		if ( (!elecType[0].toString().equals(type)) || (!elecType[1].toString().equals(type)) ) {
-//			throw new IllegalArgumentException ("INCORRECT TYPE");
-//		}
+
 		for (ElectionType elect : ElectionType.values()) {
 			if (!elect.name().equals(type))
 			{
@@ -189,8 +190,67 @@ public class DawsonElection implements Election {
 			throw new IllegalArgumentException ("TALLY MUST NOT BE NULL");
 		}
 	}
-
-
+	
+	private BallotItem[] checkItem (String...items ) {
+		if (items.length < 2)
+		{
+			throw new IllegalArgumentException ("THERE MUST BE AT LEAST 2 ITEMS");
+		}
+		BallotItem[] choices = new BallotItem[items.length];
+		
+		for (int i = 0; i < items.length; i++)
+		{
+			choices[i] = new DawsonBallotItem(items[i], items.length);
+		}
+		return choices;
+	}
+	
+	@Override
+	public String toString() {
+	    String election = (this.name + "*" + startDate.getYear() + "*" + startDate.getMonthValue()
+	    + "*" + startDate.getDayOfMonth() + "*" + endDate.getYear() + "*" + endDate.getMonthValue()
+	    + "*" + endDate.getDayOfMonth() + "*" + this.startRange + "*" + this.endRange
+	    + "*" + this.type + "*" +  ballotItems.length);  //    THIS IS NOT FINISH   <=======================================================
+	   
+	    for (int i = 0; i < ballotItems.length; i++)
+	    {
+	    	election += "\n" + ballotItems[i].toString();
+	    }
+	    return election;	
+	}
+	
+	@Override
+	public int compareTo(Election name) {
+		if (name == null)
+		{
+			throw new IllegalArgumentException("Object Name is Null");
+		}
+		int check = this.getName().compareToIgnoreCase(name.getName());
+		return check;
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		if (this == object)
+		{
+			return true;
+		}
+		if (object == null)
+		{	
+			return false;
+		}
+		if (object instanceof DawsonElection)
+		{
+			if ( ((DawsonElection)object).getName().equalsIgnoreCase(this.getName())  )
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.getName());
+	}
 }
-
-
