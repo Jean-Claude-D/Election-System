42e85d97abd6b2fd0ad0e694ba27b565b7ea38ec
m.hos <mariahossain6@gmail.com>
2017-10-02 14:49:06 -0400

    Added Some comments on the emailTest class

--- Common5/test/lib/emailTest.java
+++ Common5/test/lib/emailTest.java
@@ -6,19 +6,25 @@ public class emailTest {
 
 	public static void main(String[] args) {
 		
+		
+		//Created 2 email address String with identical UserID and diffrent 
+		//HostId
 		String email1= "mariahossain6@gmail.com";
 		String email2= "mariahosSain6@hotmail.com";
 		
-		
+		//Using the email address, we created 2 email objects to test the methods
+		//within it
 		email voter1Email = new email(email1);
 		email voter2Email= new email(email2);
 		
-		System.out.println(voter1Email.getAddress());
-		System.out.println(voter1Email.getUserId());
-		System.out.println(voter1Email.getHost());
 		
-		System.out.println(voter1Email.equals(voter2Email));
-		System.out.println(voter1Email.hashCode());
+		
+		System.out.println(voter1Email.getAddress());//Output:mariahossain6@gmail.com
+		System.out.println(voter1Email.getUserId());//Output:mariahossain6
+		System.out.println(voter1Email.getHost());//Output: gmail.com
+		
+		System.out.println(voter1Email.equals(voter2Email));//Output:False
+		System.out.println(voter1Email.hashCode());//Output: A number
 		
 		
 
