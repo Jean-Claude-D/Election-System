dc6dbf34d281267f96457cbf2d4e2ef9fb9c7e73
DesJC <jcd9930@hotmail.ca>
2017-10-02 23:24:47 -0400

    Resolve issue with DawsonVoter#setPostalCode(PostalCode)

--- ElectionSys5/src/election/business/DawsonVoter.java
+++ ElectionSys5/src/election/business/DawsonVoter.java
@@ -37,7 +37,7 @@ public class DawsonVoter implements Voter {
 	}
 	
 	public void setPostalCode(PostalCode newpostalCode) {
-		this.postalCode = new PostalCode(newpostalCode);
+		this.postalCode = new PostalCode(newpostalCode.getCode());
 	}
 
 	@Override
