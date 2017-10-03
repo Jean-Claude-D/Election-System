8bae94a9ba69bec0865b49e77b4e2c7ad5a34d0e
m.hos <mariahossain6@gmail.com>
2017-10-02 21:31:46 -0400

    Fixed Indentation and Created a 3rd DawsonItemBallot for the test class

--- ElectionSys5/src/election/business/DawsonItemBallot.java
+++ ElectionSys5/src/election/business/DawsonItemBallot.java
@@ -1,23 +1,21 @@
+package election.business;
+
+import election.business.interfaces.BallotItem;
+
 /**
  * 
  * @author Maria Hossain
  * @since Oct. 2nd 2017
  */
-
-
-package election.business;
-
-import election.business.interfaces.BallotItem;
-
 public class DawsonItemBallot implements BallotItem {
-	
-	
+
+
 	private String choiceString;
 	private int value;
 	private final int maxValue;
 	private static final long serialVersionUID = 42031768871L;
-	
-	
+
+
 	/**
 	 * 
 	 * The constructor takes in 2 parameter to create the DawsonItemBallot which
@@ -26,164 +24,164 @@ public class DawsonItemBallot implements BallotItem {
 	 * @param choiceString
 	 * @param maxValue
 	 */
-	
-	
+
+
 	public DawsonItemBallot(String choiceString, int maxValue){
-		
-		
+
+
 		this.choiceString=choiceString;
 		this.maxValue=maxValue;
 		this.value=0;
 		validateBallotItem();
-		
-		
+
+
 	}
-	
+
 	/**
 	 * 
 	 * This is the copy constructor that takes a DawsonItemBallot Object as a parameter
 	 * DawsonItemBallot objet.
 	 * @param item
 	 */
-	
+
 	public DawsonItemBallot(DawsonItemBallot item) {
-		
+
 		this.choiceString=getChoice();
 		this.value=getValue();
 		this.maxValue=getMaxValue();
 		validateBallotItem(); 
-		
+
 	}
-	
+
 	/**
 	 * 
 	 * This method makes sure that the choice is not null and that the maxValue 
 	 * is not lower than 1 
 	 */
-	
+
 	public void validateBallotItem() {
-		
+
 		if (choiceString == null && maxValue <1 ) {
-			
+
 			throw new IllegalArgumentException("not valid");
 		}
-		
+
 	}
-	
+
 	/**
 	 * This methods check that the object is of the same instance as DawsonItemBallot
 	 * 
 	 * And that the choice is the same
 	 * 
 	 */
-	
-@Override
-	
+
+	@Override
+
 	public final boolean equals(Object dawsonItemBallot){
-	
-	if(dawsonItemBallot == null) {
-		return false;
+
+		if(dawsonItemBallot == null) {
+			return false;
+		}
+
+		if (this instanceof DawsonItemBallot) {
+
+			if (this.choiceString.equals(((DawsonItemBallot)dawsonItemBallot).getChoice())) {
+
+
+				return true;
+
+
+			}
+
+		}
+		return false; 
 	}
 
-		  if (this instanceof DawsonItemBallot) {
-			  
-		  if (this.choiceString.equals(((DawsonItemBallot)dawsonItemBallot).getChoice())) {
-			  
-		    
-		      return true;
-		    
-		  
-		  }
-		 
-	}
-		  return false; 
-}
+	/**
+	 * 
+	 * This method returns the hash code of the choice.
+	 */
 
-/**
- * 
- * This method returns the hash code of the choice.
- */
+	@Override
 
-@Override
+	public int hashCode() {
 
-public int hashCode() {
-	
-	return this.getChoice().toLowerCase().hashCode();
-	
+		return this.getChoice().toLowerCase().hashCode();
+
 	}
 
-/**
- * 
- * This overriden toString() method return a string that has the value and choice
- * with a '*' delimeter. 
- * 
- * For example it returns
- * 
- * choice*value
- */
+	/**
+	 * 
+	 * This overriden toString() method return a string that has the value and choice
+	 * with a '*' delimeter. 
+	 * 
+	 * For example it returns
+	 * 
+	 * choice*value
+	 */
 
-@Override
+	@Override
 
-public String toString() {
-	
-	String choice=this.getChoice();
-	int Value=this.getValue();
-	
-	
-	
-	return (choice + "*" + Value);
-	
-	
-}
+	public String toString() {
 
-/**
- * 
- * Method set the value field to another value. It makes sure it is not greater than MaxValue
- */
+		String choice=this.getChoice();
+		int Value=this.getValue();
 
 
-	
+
+		return (choice + "*" + Value);
+
+
+	}
+
+	/**
+	 * 
+	 * Method set the value field to another value. It makes sure it is not greater than MaxValue
+	 */
+
+
+
 	public void setValue(int value) {
-		
+
 		if (!(0 <= value && value <= maxValue)) {
-			
+
 			throw new IllegalArgumentException("not valid");
 		}
-		
+
 		this.value=value;
 	}
-	
+
 	/**
 	 * 
 	 * Returns the current choice
 	 */
-	
+
 	public String getChoice() {
-		
+
 		return this.choiceString;
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * Returns the current value
 	 */
 	public int getValue() {
-		
+
 		return value;
 	}
-	
-	
+
+
 	/**
 	 * 
 	 * Returns the current MaxValue
 	 */
-	
+
 	public int getMaxValue() {
-		
+
 		return maxValue;
 	}
-	
+
 	/**
 	 * 
 	 * Returns int that represent if one choice is greater than the lexicographical order
@@ -195,34 +193,34 @@ public String toString() {
 	 * @param item
 	 * @return
 	 */
-	
+
 	public int compareTo(DawsonItemBallot item) {
-		
+
 		if(this.getChoice().compareToIgnoreCase(item.getChoice()) ==1) {
-			
+
 			return 1;
 		}
-	
-	
-	else if(this.getChoice().compareToIgnoreCase(item.getChoice()) ==1) {
-		
-		return -1;
+
+
+		else if(this.getChoice().compareToIgnoreCase(item.getChoice()) ==1) {
+
+			return -1;
+		}
+
+		else {
+
+			return 0;
+		}
+
 	}
-	
-	else {
-		
-		return 0;
-	}
-	
-}
-	
+
 	/**
 	 * 
 	 * Compare an object to the BollotItem
 	 */
-	
+
 	public int compareTo(BallotItem bItem) {
-		
+
 		return (this.compareTo(bItem));
 	}
 
--- ElectionSys5/test/DawsonItemBallotTest.java
+++ ElectionSys5/test/DawsonItemBallotTest.java
@@ -5,23 +5,35 @@ public class DawsonItemBallotTest {
 	public static void main(String[] args) {
 		
 		
+		//Variable I have created to create 3 DawsonItemBallot Object
+		
+		
 		int maxValue= 5;
 		String choice= "Salad";
 		int value= 2;
+		int value1=3;
+		String choice2= "Fries";
 		
+		
+		//Created 3 object to test the methods within the DawsonItemBallot
 		
 		DawsonItemBallot item1= new DawsonItemBallot(choice,maxValue);
 		DawsonItemBallot item2= new DawsonItemBallot(choice,maxValue);
+		DawsonItemBallot item3= new DawsonItemBallot(choice2,value1);
 		
-		System.out.println(item1.equals(null));		
 		
-		System.out.println(item1.equals(item2)); // Not Good
+		//All the methods tested within the DawsonItemBallot
+		
+		System.out.println(item1.equals(null));	//good, it returns False	
+		System.out.println(item1.equals(item3));// good
+		System.out.println(item1.equals(item2)); //  Good
 		System.out.println(item1.hashCode()); //Good
 		System.out.println(item1.toString()); // Good
+		System.out.println(item3.toString());// Good
 		
 		item1.setValue(value); //good
 		System.out.println(item1.getValue()); //good
-		
+		System.out.println(item3.getChoice());//good
 		System.out.println(item1.getChoice()); //good
 		System.out.println(item1.getMaxValue()); // good
 		
