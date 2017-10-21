6583f2defe1eddc36f5448f5d3d146df09de7bec
m.hos <mariahossain6@gmail.com>
2017-10-21 13:56:36 -0400

    Created the package with the class inside of it. Put in the method header and signiture to start the implementation

---
+++ ElectionSys5/src/election/data/ElectionFileLoader.java
@@ -0,0 +1,14 @@
+package election.data;
+
+public class ElectionFileLoader {
+	
+	public static Voter[] getVoterListFromSequentialFile(String filename) {};
+	
+	public static Election[] getElectionListFromSequentialFile(String filename) {};
+	
+	public static void setExistingTallyFromSequentialFile (String filename,Election[] elections) {};
+	
+
+
+
+}
