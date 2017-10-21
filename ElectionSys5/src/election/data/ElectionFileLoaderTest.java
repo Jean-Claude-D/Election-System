f80ddad98564e21bc5d133b6bcd859353ee0b2db
m.hos <mariahossain6@gmail.com>
2017-10-21 17:47:03 -0400

    Made corrections from the WIP merges request assigned To Jean-Claude

---
+++ ElectionSys5/src/election/data/ElectionFileLoaderTest.java
@@ -0,0 +1,28 @@
+package election.data;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ElectionFileLoaderTest {
+
+	public static void main(String[] args) throws IOException {
+
+	}// Main
+
+	public static void testGetVoterListFromSequentialFile(String fileNameTest, boolean expectedValid) {
+
+	}// End of Testing GetVoterListFromSequentialFile
+
+	public static void testGetElectionListFromSequentialFile(String fileNameTest, boolean expectedValid) {
+
+	}// End of Testing GetElectionListFromSequentialFile
+
+	public static void testsetExistingTallyFromSequentialFile(String fileNameTest, boolean expectedValid) {
+	
+	}// End of testing SetExistingTallyFromSequentialFile
+	
+
+}
