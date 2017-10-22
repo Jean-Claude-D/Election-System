7365a0c2820f76ce0af0644adaddf8c8b07656cc
DesJC <jcd9930@hotmail.ca>
2017-10-22 13:34:48 -0400

    Added GetVoterListFromSequentialFile, did some unofficial tests

--- ElectionSys5/src/election/data/ElectionFileLoaderTest.java
+++ ElectionSys5/src/election/data/ElectionFileLoaderTest.java
@@ -1,47 +1,62 @@
 package election.data;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
+import election.business.interfaces.Voter;
 
 public class ElectionFileLoaderTest {
 
-	public static void main(String[] args) throws IOException {
-		
-		
-		//Variables
-		String nameOfFile = null;
-		boolean expectedResult= false;
-		
-		
-		//The test method where we will pass the variables
-		
-		//Variable Team mate 1 
-		testGetVoterListFromSequentialFile(nameOfFile,expectedResult);
-		
-		//Variable Team mate 2
-		testGetElectionListFromSequentialFile(nameOfFile,expectedResult);
-		
-		//Variable Team mate 3 
-		testsetExistingTallyFromSequentialFile(nameOfFile,expectedResult);
-		
-	
+  public static void main(String[] args) throws IOException {
 
-	}// Main
 
-	public static void testGetVoterListFromSequentialFile(String fileNameTest, boolean expectedValid) {
+    // Variables
+    String nameOfFile = null;
+    boolean expectedResult = false;
 
-	}// End of Testing GetVoterListFromSequentialFile
 
-	public static void testGetElectionListFromSequentialFile(String fileNameTest, boolean expectedValid) {
+    // The test method where we will pass the variables
 
-	}// End of Testing GetElectionListFromSequentialFile
+    // Variable Team mate 1
+    String pathToVoterOne = "datafiles/voters1.txt";
 
-	public static void testsetExistingTallyFromSequentialFile(String fileNameTest, boolean expectedValid) {
-	
-	}// End of testing SetExistingTallyFromSequentialFile
-	
+
+
+    testGetVoterListFromSequentialFile(pathToVoterOne, true);
+
+    // Variable Team mate 2
+    // testGetElectionListFromSequentialFile(nameOfFile, expectedResult); //Uncomment those!
+
+    // Variable Team mate 3
+    // testsetExistingTallyFromSequentialFile(nameOfFile, expectedResult); //Uncomment those!
+
+
+
+  }// Main
+
+  public static void testGetVoterListFromSequentialFile(String fileNameTest,
+      boolean expectedValid) {
+    try {
+      Voter[] voters =
+          ElectionFileLoader.GetVoterListFromSequentialFile("path/leads/nowhere/at.txt");
+
+      for (Voter v : voters)
+        System.out.println(v);
+    } catch (Exception e) {
+      System.err.println("Something went wrong");
+      e.printStackTrace();
+    }
+
+
+  }// End of Testing GetVoterListFromSequentialFile
+
+  public static void testGetElectionListFromSequentialFile(String fileNameTest,
+      boolean expectedValid) {
+
+  }// End of Testing GetElectionListFromSequentialFile
+
+  public static void testsetExistingTallyFromSequentialFile(String fileNameTest,
+      boolean expectedValid) {
+
+  }// End of testing SetExistingTallyFromSequentialFile
+
 
 }
