142945f8fe18ba89cdf2116fcc8e03590f44fb32
Cao Hoang, Nguyen <nch1942@gmail.com>
2017-10-21 20:25:54 -0400

    quick fix, need to checkout back to dawelemod to fix an important issue

--- ElectionSys5/test/election/business/VoterPostalCodeComparatorTest.java
+++ ElectionSys5/test/election/business/VoterPostalCodeComparatorTest.java
@@ -1,5 +1,13 @@
 package election.business;
 
 public class VoterPostalCodeComparatorTest {
+  
+  public static void main(String[] args) {
+    
+    DawsonVoter newVoter = new DawsonVoter("Yamamoto", "Isoroku", String email, String pCode)
+    
+  }
 
+  
 }
+
