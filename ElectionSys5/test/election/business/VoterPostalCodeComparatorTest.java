75f2ad0fff64e76477c13e8bbb5e4b1b0bb4703b
Cao Hoang, Nguyen <nch1942@gmail.com>
2017-10-21 21:49:08 -0400

    continue testing VoterPostalCodeComparatorTest

--- ElectionSys5/test/election/business/VoterPostalCodeComparatorTest.java
+++ ElectionSys5/test/election/business/VoterPostalCodeComparatorTest.java
@@ -4,10 +4,14 @@ public class VoterPostalCodeComparatorTest {
   
   public static void main(String[] args) {
     
-    DawsonVoter newVoter = new DawsonVoter("Yamamoto", "Isoroku", String email, String pCode)
+    VoterPostalCodeComparator check = new VoterPostalCodeComparator();
     
-  }
+    DawsonVoter voter1 = new DawsonVoter("Yamamoto", "Isoroku", "Rising_Sun@Banzai.Kamikaze.jp", "H5J 2G9");
+    
+    DawsonVoter voter2 = new DawsonVoter("Takeda", "Shingen", "Rise_Of_Samurai@Katana.jp", "J5B 2Y7");
+    
+    check.compare(voter1, voter2);
 
-  
+  }
 }
 
