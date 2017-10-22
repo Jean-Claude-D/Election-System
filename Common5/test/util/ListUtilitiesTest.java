9c2f0ad58e1cb90dd47b1d66df410227ba822ebf
Gor_F <felicia.gorgatchov@gmail.com>
2017-10-22 00:46:56 -0700

    Create ListUtilitiesTest skeleton

---
+++ Common5/test/util/ListUtilitiesTest.java
@@ -0,0 +1,34 @@
+package util;
+
+public class ListUtilitiesTest {
+	
+	public static void main (String [] args) {
+		
+//----- Variables for Jean-Claude ---------------------------------------------------------------------------------
+		
+//----- Variables for Cao Hoang -----------------------------------------------------------------------------------
+		
+//----- Variables for Felicia -------------------------------------------------------------------------------------		
+		
+	}//end main
+	
+	
+	
+	
+//TEST METHODS -------------------------------------------------------------------------------------------------
+	private static void sortTest(Comparable[] list, boolean expectedResult) {
+		
+	}
+	
+	
+	private static void mergeTest(Comparable[] list1, Comparable[] list2, String test, boolean expectedResult) {
+		
+	}
+	
+	
+	private static void comparatorTest() {
+		
+	}
+	
+	
+}//end of test class -------------------------------------------------------------------------------------------
