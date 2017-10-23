e6dc23d9d744da99b6d42d74cdd2cf4df01949fb
m.hos <mariahossain6@gmail.com>
2017-10-22 22:34:31 -0400

    Just created the VoterNameComparator class

---
+++ ElectionSys5/src/election/business/VoterNameComparator.java
@@ -0,0 +1,11 @@
+package election.business;
+
+import java.util.Comparator;
+
+import election.business.interfaces.Voter;
+
+public class VoterNameComparator implements Comparator<Voter> {
+	
+	
+
+}
