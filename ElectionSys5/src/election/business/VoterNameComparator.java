9f9ccaa85c26811fe77253e6f6f6b44423b35996
m.hos <mariahossain6@gmail.com>
2017-10-23 11:57:48 -0400

    Done with the VoterNameComp just needt to test it

--- ElectionSys5/src/election/business/VoterNameComparator.java
+++ ElectionSys5/src/election/business/VoterNameComparator.java
@@ -1,40 +1,27 @@
 package election.business;
 
 import java.util.Comparator;
-
 import election.business.interfaces.Voter;
 
 public class VoterNameComparator implements Comparator<Voter> {
 
-	@Override
-	public int compare(Voter voter1, Voter voter2) {
+  @Override
+  public int compare(Voter voter1, Voter voter2) {
 
-		if (voter1.getName().equals(voter2.getName())) {
+    if (voter1.getName().compareTo(voter2.getName()) == 0) {
 
-			if (voter1.getName().compareTo(voter2.getName()) == 0){
+      return voter1.compareTo(voter2);
 
-				return 0;
-			}
+    }
 
-			else if(voter1.getName().compareTo(voter2.getName()) == 1) {
+    else {
 
-				return 1;
-			}
+      return voter1.getName().compareTo(voter2.getName());
+    }
 
-			else {
 
-				return -1;
-			}
-		}
-		
-		//If it does not go in the if statement it is because the name of the voters are not the same so it will
-		//Automatically return -1
-		return -1;
-		
-		
-	}
+  }
 
 }
-
 
 
