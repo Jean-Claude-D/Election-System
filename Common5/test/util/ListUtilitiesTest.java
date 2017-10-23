9cab1497aeef738d988d1f105f30f54e40289192
DesJC <jcd9930@hotmail.ca>
2017-10-23 09:52:50 -0400

    Begin test for merge ListUtilities

--- Common5/test/util/ListUtilitiesTest.java
+++ Common5/test/util/ListUtilitiesTest.java
@@ -3,37 +3,42 @@ package util;
 import java.util.Comparator;
 
 public class ListUtilitiesTest {
-	
-	public static void main (String [] args) {
-		
-//----- Variables for Jean-Claude ---------------------------------------------------------------------------------
-		
-//----- Variables for Cao Hoang -----------------------------------------------------------------------------------
-		
-//----- Variables for Felicia -------------------------------------------------------------------------------------		
-		
-	}//end main
-	
-	
-	
-	
-//TEST METHODS -------------------------------------------------------------------------------------------------
-	//part 4
-	@SuppressWarnings({"rawtypes","unchecked"})
-	private static void sortTest(Comparable[] list, boolean expectedResult) {
-		
-	}
-	
-	@SuppressWarnings({"rawtypes","unchecked"})
-	private static void mergeTest(Comparable[] list1, Comparable[] list2, String test, boolean expectedResult) {
-		
-	}
-	
-	//comparator, part 5
-	@SuppressWarnings({"rawtypes","unchecked"})
-	private static void sortTest(Comparable[] list, Comparator sortOrder, boolean expectedResult) {
-		
-	}
-	
-	
-}//end of test class -------------------------------------------------------------------------------------------
+
+  public static void main(String[] args) {
+
+    // ----- Variables for Jean-Claude
+    // ---------------------------------------------------------------------------------
+
+    // ----- Variables for Cao Hoang
+    // -----------------------------------------------------------------------------------
+
+    // ----- Variables for Felicia
+    // -------------------------------------------------------------------------------------
+
+  }// end main
+
+
+
+  // TEST METHODS
+  // -------------------------------------------------------------------------------------------------
+  // part 4
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private static void sortTest(Comparable[] list, boolean expectedResult) {
+
+  }
+
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private static void mergeTest(Comparable[] list1, Comparable[] list2, Comparable[] expectedList,
+      String duplicateFileNameTest, boolean expectedResult, Class expectedException) {
+
+  }
+
+  // comparator, part 5
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  private static void sortTest(Comparable[] list, Comparator sortOrder, boolean expectedResult) {
+
+  }
+
+
+}// end of test class
+ // -------------------------------------------------------------------------------------------
