4dbb142005a26823cb6f080b0ca52e23853aad86
DesJC <jcd9930@hotmail.ca>
2017-10-24 02:18:02 -0400

    Fix the bug in merge in ListUtilities, the misuse of addAll()

--- Common5/test/util/ListUtilitiesTest.java
+++ Common5/test/util/ListUtilitiesTest.java
@@ -2,9 +2,6 @@ package util;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import lib.Email;
-import lib.Name;
-import lib.PostalCode;
 
 public class ListUtilitiesTest {
 
@@ -34,7 +31,7 @@ public class ListUtilitiesTest {
     c2[5] = 324;
     c2[6] = 383;
     c2[7] = 669;
-    c2[8] = 811;//
+    c2[8] = 811;
     c2[9] = 893;
 
 
@@ -81,11 +78,32 @@ public class ListUtilitiesTest {
     ca[16] = 757;
     ca[17] = 778;
     ca[18] = 811;
-    ca[19] = 897;
+    ca[19] = 893;
+
+    Comparable[] cb = new Comparable[19];
+    cb[0] = -936;
+    cb[1] = -876;
+    cb[2] = -615;
+    cb[3] = -501;
+    cb[4] = -404;
+    cb[5] = -268;
+    cb[6] = -96;
+    cb[7] = 22;
+    cb[8] = 30;
+    cb[9] = 338;
+    cb[10] = 520;
+    cb[11] = 536;
+    cb[12] = 568;
+    cb[13] = 607;
+    cb[14] = 757;
+    cb[15] = 778;
+    cb[16] = 785;
+    cb[17] = 790;
+    cb[18] = 893;
 
     mergeTest(c1, c2, ca, "datafiles/duplicates/duplicates.txt", true, null);
 
-    // mergeTest(c1, c3, null, null, false, null);
+    mergeTest(c1, c3, cb, "datafiles/duplicate/duplicate.txt", true, null);
     //
     // mergeTest(c1, c2Copy, null, null, false, null);
     //
@@ -94,94 +112,79 @@ public class ListUtilitiesTest {
     // mergeTest(c2, c2Copy, null, null, false, null);
     //
     // mergeTest(c3, c2Copy, null, null, false, null);
-
-    // ----- Variables for Cao Hoang
-    // -----------------------------------------------------------------------------------
-
-    Name person1 = new Name("Jean-Claude", "French");
-    Name person2 = new Name("Cao Hoang", "Vietnam");
-    Name person3 = new Name("Maria", "Morocco");
-    Name person4 = new Name("Felicia", "Bulgaria");
-    Name person5 = new Name("Our-Team", "Canada");
-    Name person6 = new Name("Bob", "French");
-
-    Name[] listNameTest = new Name[] {person1, person2, person3, person4, person5, person6};
-
-    Name[] listNameCheck = new Name[] {person4, person5, person6, person1, person3, person2};
-
-    sortTest(listNameTest, new NameByFirstComparator(), listNameCheck, true);
-
-    // ----- Variables for Felicia
-    // -------------------------------------------------------------------------------------
-
-    boolean expectValid = true;
-    boolean expectInvalid = false;
-
-    Comparable[] predictException = new Comparable[10];
-
-    Name fel = new Name("Felicia", "Gorgatchov");
-    Name ivo = new Name("Ivaylo", "Gorgatchov");
-    Name jc = new Name("Jean-Claude", "Desrosiers");
-    Name maria = new Name("Maria", "Hossain");
-    Name hoang = new Name("Cao Hoang", "Nguyen");
-    Name[] nameSortTestGood1 = {maria, ivo, hoang, jc, fel};
-    Name[] nameSortTestGood2 = {hoang, fel, maria, jc, ivo};
-    Name[] nameSortTestNotFull = {fel, null, jc, null};
-    Name[] nameSortTestNull = {null, null, null, null};
-
-    Name[] namePrediction = {jc, fel, ivo, maria, hoang};
-
-    PostalCode pc1 = new PostalCode("J4X 2G3");
-    PostalCode pc2 = new PostalCode("H3H1M6");
-    PostalCode pc3 = new PostalCode("H3H 5B2");
-    PostalCode pc4 = new PostalCode("X0B1K0");
-    PostalCode pc5 = new PostalCode("V5K 0A4");
-    PostalCode[] pcSortTestGood = {pc4, pc3, pc5, pc2, pc1};
-    PostalCode[] pcSortTestGood2 = {pc4, pc2, pc3, pc5, pc1};
-    PostalCode[] pcSortTestNotFull = {pc3, null, pc1, null};
-    PostalCode[] pcSortTestNull = {null, null, null, null, null};
-
-    PostalCode[] postalCodePrediction = {pc2, pc3, pc1, pc5, pc4};
-
-    Email email1 = new Email("felicia.gorgatchov@gmail.com"); // 2
-    Email email2 = new Email("rick_C-137@ricksanchez.com"); // 5
-    Email email3 = new Email("mclovin81@memes.fr"); // 3
-    Email email4 = new Email("12zebulon34@bruh.uk");// 1
-    Email email5 = new Email("78hola@mexico.mx"); // 4
-    Email email6 = new Email("42@whatislife.com"); // 7
-    Email email7 = new Email("zebulon@somehostID.ca"); // 6
-
-    Email[] emailSortTestGood = {email1, email2, email3, email4, email5, email6, email7};
-    Email[] emailSortTestGood2 = {email3, email5, email1, email7, email2, email6};
-    Email[] emailSortTestGood3 = {email1, email2, email3, email7};
-    Email[] emailSortTestGood4 = {email4, email5, email6};
-    Email[] emailSortTestNotFull = {email1, email5, email4, email7, email2, null, email3};
-    Email[] emailSortTestNull = {null, null, null, null, null};
-
-    Email[] emailPrediction = {email4, email1, email3, email5, email2, email7, email6};
-
-    System.out.println(
-        "\n-------NAME------------------------------------------------------------------------------------------------------------------");
-    sortTest(nameSortTestGood1, namePrediction, expectValid);
-    sortTest(nameSortTestGood2, namePrediction, expectValid);
-    sortTest(nameSortTestNotFull, predictException, expectInvalid);
-    sortTest(nameSortTestNull, predictException, expectInvalid);
-
-    System.out.println(
-        "\n-------POSTALCODE-------------------------------------------------------------------------------------------------------------------");
-    sortTest(pcSortTestGood, postalCodePrediction, expectValid);
-    sortTest(pcSortTestGood2, postalCodePrediction, expectValid);
-    sortTest(pcSortTestNotFull, predictException, expectInvalid);
-    sortTest(pcSortTestNull, predictException, expectInvalid);
-
-    System.out.println(
-        "\n-------EMAIL------------------------------------------------------------------------------------------------------------------");
-    sortTest(emailSortTestGood, emailPrediction, expectValid);
-    sortTest(emailSortTestGood2, emailPrediction, expectValid);
-    sortTest(emailSortTestGood3, emailPrediction, expectValid);
-    sortTest(emailSortTestGood4, emailPrediction, expectValid);
-    sortTest(emailSortTestNotFull, predictException, expectInvalid);
-    sortTest(emailSortTestNull, predictException, expectInvalid);
+    /*
+     * // ----- Variables for Cao Hoang //
+     * -----------------------------------------------------------------------------------
+     * 
+     * Name person1 = new Name("Jean-Claude", "French"); Name person2 = new Name("Cao Hoang",
+     * "Vietnam"); Name person3 = new Name("Maria", "Morocco"); Name person4 = new Name("Felicia",
+     * "Bulgaria"); Name person5 = new Name("Our-Team", "Canada"); Name person6 = new Name("Bob",
+     * "French");
+     * 
+     * Name[] listNameTest = new Name[] {person1, person2, person3, person4, person5, person6};
+     * 
+     * Name[] listNameCheck = new Name[] {person4, person5, person6, person1, person3, person2};
+     * 
+     * sortTest(listNameTest, new NameByFirstComparator(), listNameCheck, true);
+     * 
+     * // ----- Variables for Felicia //
+     * -------------------------------------------------------------------------------------
+     * 
+     * boolean expectValid = true; boolean expectInvalid = false;
+     * 
+     * Comparable[] predictException = new Comparable[10];
+     * 
+     * Name fel = new Name("Felicia", "Gorgatchov"); Name ivo = new Name("Ivaylo", "Gorgatchov");
+     * Name jc = new Name("Jean-Claude", "Desrosiers"); Name maria = new Name("Maria", "Hossain");
+     * Name hoang = new Name("Cao Hoang", "Nguyen"); Name[] nameSortTestGood1 = {maria, ivo, hoang,
+     * jc, fel}; Name[] nameSortTestGood2 = {hoang, fel, maria, jc, ivo}; Name[] nameSortTestNotFull
+     * = {fel, null, jc, null}; Name[] nameSortTestNull = {null, null, null, null};
+     * 
+     * Name[] namePrediction = {jc, fel, ivo, maria, hoang};
+     * 
+     * PostalCode pc1 = new PostalCode("J4X 2G3"); PostalCode pc2 = new PostalCode("H3H1M6");
+     * PostalCode pc3 = new PostalCode("H3H 5B2"); PostalCode pc4 = new PostalCode("X0B1K0");
+     * PostalCode pc5 = new PostalCode("V5K 0A4"); PostalCode[] pcSortTestGood = {pc4, pc3, pc5,
+     * pc2, pc1}; PostalCode[] pcSortTestGood2 = {pc4, pc2, pc3, pc5, pc1}; PostalCode[]
+     * pcSortTestNotFull = {pc3, null, pc1, null}; PostalCode[] pcSortTestNull = {null, null, null,
+     * null, null};
+     * 
+     * PostalCode[] postalCodePrediction = {pc2, pc3, pc1, pc5, pc4};
+     * 
+     * Email email1 = new Email("felicia.gorgatchov@gmail.com"); // 2 Email email2 = new
+     * Email("rick_C-137@ricksanchez.com"); // 5 Email email3 = new Email("mclovin81@memes.fr"); //
+     * 3 Email email4 = new Email("12zebulon34@bruh.uk");// 1 Email email5 = new
+     * Email("78hola@mexico.mx"); // 4 Email email6 = new Email("42@whatislife.com"); // 7 Email
+     * email7 = new Email("zebulon@somehostID.ca"); // 6
+     * 
+     * Email[] emailSortTestGood = {email1, email2, email3, email4, email5, email6, email7}; Email[]
+     * emailSortTestGood2 = {email3, email5, email1, email7, email2, email6}; Email[]
+     * emailSortTestGood3 = {email1, email2, email3, email7}; Email[] emailSortTestGood4 = {email4,
+     * email5, email6}; Email[] emailSortTestNotFull = {email1, email5, email4, email7, email2,
+     * null, email3}; Email[] emailSortTestNull = {null, null, null, null, null};
+     * 
+     * Email[] emailPrediction = {email4, email1, email3, email5, email2, email7, email6};
+     * 
+     * System.out.println(
+     * "\n-------NAME------------------------------------------------------------------------------------------------------------------"
+     * ); sortTest(nameSortTestGood1, namePrediction, expectValid); sortTest(nameSortTestGood2,
+     * namePrediction, expectValid); sortTest(nameSortTestNotFull, predictException, expectInvalid);
+     * sortTest(nameSortTestNull, predictException, expectInvalid);
+     * 
+     * System.out.println(
+     * "\n-------POSTALCODE-------------------------------------------------------------------------------------------------------------------"
+     * ); sortTest(pcSortTestGood, postalCodePrediction, expectValid); sortTest(pcSortTestGood2,
+     * postalCodePrediction, expectValid); sortTest(pcSortTestNotFull, predictException,
+     * expectInvalid); sortTest(pcSortTestNull, predictException, expectInvalid);
+     * 
+     * System.out.println(
+     * "\n-------EMAIL------------------------------------------------------------------------------------------------------------------"
+     * ); sortTest(emailSortTestGood, emailPrediction, expectValid); sortTest(emailSortTestGood2,
+     * emailPrediction, expectValid); sortTest(emailSortTestGood3, emailPrediction, expectValid);
+     * sortTest(emailSortTestGood4, emailPrediction, expectValid); sortTest(emailSortTestNotFull,
+     * predictException, expectInvalid); sortTest(emailSortTestNull, predictException,
+     * expectInvalid);
+     */
 
   }// end main
 
@@ -353,8 +356,8 @@ public class ListUtilitiesTest {
 
       Comparable[] list3 = ListUtilities.merge(list1, list2, duplicateFileNameTest);
 
-      for (Comparable c : list3)
-        System.out.println(c);
+      for (int i = 0; i < list3.length; i++)
+        System.out.println("at [" + i + "] :" + list3[i].toString());
 
       if (list3.length == expectedList.length && expectedValid) {
         for (int i = 0; i < list3.length; i++) {
