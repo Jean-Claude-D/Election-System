059e6306aa7c0904586d4c911e83c606d000dc18
Gor_F <felicia.gorgatchov@gmail.com>
2017-10-24 17:14:58 -0700

    code sortVoters further

--- ElectionSys5/src/election/data/SortMergeApp.java
+++ ElectionSys5/src/election/data/SortMergeApp.java
@@ -1,58 +1,69 @@
 package election.data;
 
-
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import util.ListUtilities;
 
 public class SortMergeApp {
-
-  private Path path = new Path("\\ElectionSys\\ElectionSys5\\datafiles");
-
-  /*
-   * load(read) 9 voter files and sort and put in new file datafiles\sorted\sorteVotersX where X is
-   * the number of the voter file
-   */
-  public static void sortVoters() {
-    Paths.get(path);
-  }
-
-
-  /*
-   * merge them together (first merges with 2 to make "Total", total merges with 3 and updates
-   * total, total merges with 4, etc. Total now contains the 4 first voters place the final result
-   * in datafiles\database\voters.txt duplicate voters must be stored in
-   * datafiles\database\duplicatevoters.txt
-   */
-  public static void mergeVoters(Path sortedVorters) {
-
-  }
-
-
-  public static void sortElection() {
-
-    // Created a path to which the Election Files will be called
-    Path p = Paths.get(path);
-
-    // Turned this list into a arrayList
-    List<String> election = Files.readAllLines(p);
-
-
-    // Turned the list into an Array of size election.size
-    String[] electionToArray = election.toArray(new String[election.size()]);
-
-    // Called the sort methods onto the array
-    ListUtilities.sort(electionToArray);
-
-  }
-
-
-  /*
-   * 
-   */
-
-
+	
+	private static String path = "/ElectionSys/ElectionSys5/datafiles/unsorted";
+	static List<String> arrayOfVoters = new ArrayList<>();
+	
+	
+	/*
+	 * load(read) 9 voter files and sort and put in new file datafiles\sorted\sorteVotersX where X 
+	 * is the number of the voter file
+	 */
+	public static void sortVoters() throws IOException{
+		//read and place in array
+		//sort array
+		//put sorted array in new file
+		//repeat
+		ListUtilities a = new ListUtilities();
+		File voters = new File(path);
+		String[] listOfVoters = voters.list();
+		for(int i = 0; i < listOfVoters.length; i++) {
+			//checkif file nate containes "voters"
+			if(listOfVoters[i].matches("voters")) {
+				path = path + "/voters" + i + ".txt";
+				File voterFile = new File(path);
+				if (voterFile.exists()) {
+					Path p = Paths.get(path);
+					arrayOfVoters = Files.readAllLines(p);
+					String[] sortedArray = arrayOfVoters.toArray(new String[arrayOfVoters.size()]);
+					ListUtilities.sort(sortedArray);
+				}
+				
+			}
+			//call this method
+			ElectionFileLoader.GetVoterListFromSequentialFile(listOfVoters[i]);
+		}
+		
+		
+		
+	}
+	
+	
+	/*
+	 * merge them together (first merges with 2 to make "Total", total merges with 3 and updates total, total merges with 4, etc. 
+	 * Total now contains the 4 first voters
+	 * place the final result in datafiles\database\voters.txt
+	 * duplicate voters must be stored in datafiles\database\duplicatevoters.txt
+	 */
+	public static void mergeVoters(Path sortedVorters) {
+		
+	}
+	
+	
+	/*
+	 * 
+	 */
+	
+	
+	
 }
 
