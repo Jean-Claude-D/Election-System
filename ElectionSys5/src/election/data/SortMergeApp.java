b19db64ec421527545f56b0ae94d07782dda40b6
Gor_F <felicia.gorgatchov@gmail.com>
2017-10-24 17:14:58 -0700

    code sortVoters further

--- ElectionSys5/src/election/data/SortMergeApp.java
+++ ElectionSys5/src/election/data/SortMergeApp.java
@@ -1,18 +1,50 @@
 package election.data;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SortMergeApp {
 	
-	private Path path= new Path( "\\ElectionSys\\ElectionSys5\\datafiles");
+	private static String path = "/ElectionSys/ElectionSys5/datafiles/unsorted";
+	static List<String> arrayOfVoters = new ArrayList<>();
+	
 	
 	/*
 	 * load(read) 9 voter files and sort and put in new file datafiles\sorted\sorteVotersX where X 
 	 * is the number of the voter file
 	 */
-	public static void sortVoters() {
-		Paths.get(path);
+	public static void sortVoters() throws IOException{
+		//read and place in array
+		//sort array
+		//put sorted array in new file
+		//repeat
+		ListUtilities a = new ListUtilities();
+		File voters = new File(path);
+		String[] listOfVoters = voters.list();
+		for(int i = 0; i < listOfVoters.length; i++) {
+			//checkif file nate containes "voters"
+			if(listOfVoters[i].matches("voters")) {
+				path = path + "/voters" + i + ".txt";
+				File voterFile = new File(path);
+				if (voterFile.exists()) {
+					Path p = Paths.get(path);
+					arrayOfVoters = Files.readAllLines(p);
+					String[] sortedArray = arrayOfVoters.toArray(new String[arrayOfVoters.size()]);
+					ListUtilities.sort(sortedArray);
+				}
+				
+			}
+			//call this method
+			ElectionFileLoader.GetVoterListFromSequentialFile(listOfVoters[i]);
+		}
+		
+		
+		
 	}
 	
 	
