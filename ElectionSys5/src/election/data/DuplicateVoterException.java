1e32fb7a141f636c3947257c03abe36abd66ecc1
DesJC <jcd9930@hotmail.ca>
2017-10-31 12:53:25 -0400

    Add class DuplicateVoterException into election.data

---
+++ ElectionSys5/src/election/data/DuplicateVoterException.java
@@ -0,0 +1,29 @@
+package election.data;
+
+public class DuplicateVoterException extends Exception {
+
+	public DuplicateVoterException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DuplicateVoterException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DuplicateVoterException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DuplicateVoterException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DuplicateVoterException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
