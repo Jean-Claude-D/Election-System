d342fef0cf676ee97e1dd59c882253a861696cf2
DesJC <jcd9930@hotmail.ca>
2017-10-31 13:15:00 -0400

    Replace super class Ctors in DuplicateVoterException

--- ElectionSys5/src/election/data/DuplicateVoterException.java
+++ ElectionSys5/src/election/data/DuplicateVoterException.java
@@ -2,28 +2,32 @@ package election.data;
 
 public class DuplicateVoterException extends Exception {
 
-	public DuplicateVoterException() {
-		// TODO Auto-generated constructor stub
-	}
+  /**
+   * Used to identify a class' version to avoid problems during deserialization
+   */
+  private static final long serialVersionUID = 1L;
 
-	public DuplicateVoterException(String arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
+  private static final String DEFAULT = "defaultMsg";
 
-	public DuplicateVoterException(Throwable arg0) {
-		super(arg0);
-		// TODO Auto-generated constructor stub
-	}
+  public DuplicateVoterException() {
+    super(DEFAULT);
+  }
 
-	public DuplicateVoterException(String arg0, Throwable arg1) {
-		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
-	}
+  public DuplicateVoterException(String message) {
+    super(message == null || message.isEmpty() ? DEFAULT : message);
+  }
 
-	public DuplicateVoterException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
-		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
-	}
+  public DuplicateVoterException(Throwable cause) {
+    super(cause == null ? DEFAULT : cause.toString(), cause);
+  }
+
+  public DuplicateVoterException(String message, Throwable cause) {
+    super(message == null || message.isEmpty() ? DEFAULT : message, cause);
+  }
+
+  public DuplicateVoterException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+    super(arg0, arg1, arg2, arg3);
+    // TODO Auto-generated constructor stub
+  }
 
 }
