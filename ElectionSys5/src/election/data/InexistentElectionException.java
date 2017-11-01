43c54df4c4362aab03a7da636e90b2d97214f7aa
m.hos <mariahossain6@gmail.com>
2017-11-01 13:06:03 -0400

    Not sure if this icorrect because I have essentially used the same code as the DuplicateVoterException

--- ElectionSys5/src/election/data/InexistentElectionException.java
+++ ElectionSys5/src/election/data/InexistentElectionException.java
@@ -4,6 +4,35 @@ public class InexistentElectionException extends Exception {
 
   private static final long serialVersionUID = 1L;
 
+  private static final String DEFAULT = "This election does not exist.";
 
+  public InexistentElectionException() {
+    super(DEFAULT);
+  }
+
+  public InexistentElectionException(String message) {
+
+    super(message == null || message.isEmpty() ? DEFAULT : message);
+  }
+
+  public InexistentElectionException(Throwable cause) {
+
+    super(cause == null || cause.getMessage().isEmpty() || cause.getMessage() == null ? DEFAULT
+        : cause.toString(), cause);
+  }
+
+  public InexistentElectionException(String message, Throwable cause) {
+    super(message == null || message.isEmpty() ? DEFAULT : message, cause);
+  }
+
+  protected InexistentElectionException(String message, Throwable cause, boolean enableSuppress,
+      boolean writableStackTrace) {
+    super((message == null || message.isEmpty() ? DEFAULT : message), cause, enableSuppress,
+        writableStackTrace);
+  }
+
+  public String getLocalizedMessage() {
+    return ("InexistentElectionException : " + this.getMessage());
+  }
 
 }
