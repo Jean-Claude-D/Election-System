690e229b01f1fac76de0f2f806c6f0252c0ccb23
Cao Hoang, Nguyen <nch1942@gmail.com>
2017-11-10 19:54:51 -0500

    setting up all the needed files for phase3

--- ElectionSys5/src/election/data/DuplicateVoterException.java
+++ ElectionSys5/src/election/data/DuplicateVoterException.java
@@ -1,87 +1,19 @@
 package election.data;
 
 /**
- * Is thrown when an attempt to save a Voter object already in the database is made.
+ * PROVIDED CODE This exception signals that the provided voter email exists in the database
+ * already.
  * 
- * @author DesJC
- *
+ * @author Maja, Jaya
  */
 public class DuplicateVoterException extends Exception {
+  private static final long serialVersionUID = 42031768871L;
 
-  /**
-   * The identifier of the class' version to avoid problems during deserialization.
-   */
-  private static final long serialVersionUID = 1L;
-
-
-  /**
-   * The default message used in case a message parameter is null or empty.
-   */
-  private static final String DEFAULT = "There cannot be two identical Voter objects";
-
-  /**
-   * Constructs a new DuplicateVoterException with the DEFAULT.
-   */
   public DuplicateVoterException() {
-    super(DEFAULT);
+    super("The provided Email address is associated with an existing voter.");
   }
 
-  /**
-   * Constructs a new DuplicateVoterException with the message parameter.
-   * 
-   * @param message the String message for this DuplicateVoterException, if non-null and non-empty,
-   *        else the DEFAULT is used.
-   */
   public DuplicateVoterException(String message) {
-    // If message is empty, replace it with DEFAULT
-    super(message == null || message.isEmpty() ? DEFAULT : message);
+    super(message);
   }
-
-  /**
-   * Constructs a new DuplicateVoterException with the cause parameter.
-   * 
-   * @param cause the Throwable object that caused this DuplicateVoterException to be instantiated.
-   */
-  public DuplicateVoterException(Throwable cause) {
-    // If cause's message is empty, replace it with DEFAULT
-    super(cause == null || cause.getMessage().isEmpty() || cause.getMessage() == null ? DEFAULT
-        : cause.toString(), cause);
-  }
-
-  /**
-   * Constructs a new DuplicateVoterException with the message and cause parameters.
-   * 
-   * @param message the String message for this DuplicateVoterException, if non-null and non-empty,
-   *        else the DEFAULT is used.
-   * @param cause the Throwable object that caused this DuplicateVoterException to be instantiated.
-   */
-  public DuplicateVoterException(String message, Throwable cause) {
-    super(message == null || message.isEmpty() ? DEFAULT : message, cause);
-  }
-
-  /**
-   * Constructs a new DuplicateVoterException with the message, cause, enableSuppress and
-   * writableStackTrace parameters.
-   * 
-   * @param message message the String message for this DuplicateVoterException, if non-null and
-   *        non-empty, else the DEFAULT is used.
-   * @param cause the Throwable object that caused this DuplicateVoterException to be instantiated.
-   * @param enableSuppress whether this DuplicateVoterException is suppressed or not. If this object
-   *        is suppressed, it may not always be thrown when multiple exceptions occur.
-   * @param writableStackTrace whether this DuplicateVoterException has a stackTrace or not.
-   */
-  protected DuplicateVoterException(String message, Throwable cause, boolean enableSuppress,
-      boolean writableStackTrace) {
-    super((message == null || message.isEmpty() ? DEFAULT : message), cause, enableSuppress,
-        writableStackTrace);
-  }
-
-
-  /**
-   * Returns a localized description of this DuplicateVoterException
-   */
-  public String getLocalizedMessage() {
-    return ("DuplicateVoterException : " + this.getMessage());
-  }
-
 }
