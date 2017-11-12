744ddd81594eafd281019368de37577423d4b939
m.hos <mariahossain6@gmail.com>
2017-11-12 14:41:39 -0500

    Created the getElection Method. WIP. Has a lot of compiler that needs to be resolved

--- ElectionSys5/src/election/data/ElectionListDB.java
+++ ElectionSys5/src/election/data/ElectionListDB.java
@@ -2,12 +2,13 @@ package election.data;
 
 import java.io.IOException;
 import java.util.List;
+import election.business.DawsonElectionFactory;
 import election.business.interfaces.Election;
 import election.business.interfaces.ElectionFactory;
 import election.data.interfaces.ElectionDAO;
 import election.data.interfaces.InexistentElectionException;
 import election.data.interfaces.ListPersistenceObject;
-import election.data.*;
+import util.ListUtilities;
 
 public class ElectionListDB implements ElectionDAO {
 
@@ -18,16 +19,46 @@ public class ElectionListDB implements ElectionDAO {
 
   public ElectionListDB(ListPersistenceObject listPersistenceObject) {}
 
-  public ElectionListDB (ListPersistenceObject
-      listPersistenceObject, ElectionFactory factory)
+  public ElectionListDB(ListPersistenceObject listPersistenceObject, ElectionFactory factory) {}
 
 
   @Override
   public void disconnect() throws IOException {
 
 
-    saveElectionDatabase(database);
+    listPersistenceObject.saveElectionDatabase(database);
     database = null;
+
+  }
+
+  @Override
+  public Election getElection(String name) throws InexistentElectionException {
+
+    String type = null;
+    int startYear = 0;
+    int startMonth = 0;
+    int startDay = 0;
+    int endYear = 0;
+    int endMonth = 0;
+    int endDay = 0;
+    String startRange = null;
+    String endRange = null;
+    String choice = null;
+
+    Election dummy = DawsonElectionFactory.DAWSON_ELECTION.getElectionInstance(name, type,
+        startYear, startMonth, startDay, endYear, endMonth, endDay, startRange, endRange, choice);
+
+
+    int index = ListUtilities.binarySearch(database, dummy);
+
+    if (index == -1) {
+
+      throw new InexistentElectionException("This election does not exist.");
+
+    }
+
+
+    return database.get(index);
 
   }
 
@@ -36,5 +67,3 @@ public class ElectionListDB implements ElectionDAO {
 }
 
 
-
-}
