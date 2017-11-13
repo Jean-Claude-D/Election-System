ccc06941cf6bf93845d967c70e7302784b1e5c8d
Gor_F <felicia.gorgatchov@gmail.com>
2017-11-12 16:30:40 -0800

    Add InexistentVoterException class

--- ElectionSys5/src/election/data/InexistentVoterException.java
+++ ElectionSys5/src/election/data/InexistentVoterException.java
@@ -1,5 +1,29 @@
 package election.data;
 
-public class InexistentVoterException {
+/**
+ * Exception thrown when a voter profile does not exist
+ * @author Felicia Gorgatchov
+ *
+ */
 
+public class InexistentVoterException extends Exception{
+
+  private static final long serialVersionUID = 42031768871L;
+  private static final String original = "The voter you are looking for unfortunately does not exist in our system database.";
+  
+  /**
+   * Default constructor, prints the default message when called
+   */
+  
+  public InexistentVoterException() {
+    super(original);
+  }
+
+  /**
+   * constructor, take a string parameter and prints that. If no parameter is given, it will print the default message.
+   */
+  public InexistentVoterException(String message) {
+    super(message == null || message.isEmpty() ? original : message);
+  }
+  
 }
