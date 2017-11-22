6671bfcba555462a7ae65a4e8694f6d1921fcbd1
m.hos <mariahossain6@gmail.com>
2017-11-22 06:58:36 -0500

    Able to call all the main method. Just need to add notes and javadoc

--- ElectionSys5/test/runner/ElectionSysTestRunner.java
+++ ElectionSys5/test/runner/ElectionSysTestRunner.java
@@ -1,5 +1,14 @@
 package runner;
 
+import java.io.IOException;
+import election.business.DawsonBallotItemTest;
+import election.business.DawsonElectionTest;
+import election.business.DawsonVoterTest;
+import election.business.VoterNameComparatorTest;
+import election.business.VoterPostalCodeComparatorTest;
+import election.data.ElectionFileLoaderTest;
+import election.data.ElectionListDBTest;
+import election.data.VoterListDBTest;
 import lib.EmailTest;
 import lib.NameTest;
 import lib.PostalCodeTest;
@@ -7,25 +16,26 @@ import util.ListUtilitiesTest;
 
 public class ElectionSysTestRunner {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
-    System.out.println("Common5 Project Test Cases");
+    System.out.println("=================Common5 Project Test Cases=================");
+    System.out.println();
     System.out.println(
 
-        "All the below test cases are from the test/lib and test/util in the Common5 Project");
+        "****All the below test cases are from the test/lib and test/util in the Common5 Project****");
 
     System.out.println();
     System.out.println();
 
     System.out.println(
-        "The following test cases is testing the EMAIL class. The test class is EmailTest.java");
+        "****The following test cases is testing the EMAIL class. The test class is EmailTest.java****");
     System.out.println();
 
 
     EmailTest.main(args);
 
     System.out.println(
-        "The following test cases is testing the NAME class. The test class is NameTest.java");
+        "****The following test cases is testing the NAME class. The test class is NameTest.java****");
     System.out.println();
     NameTest.main(args);
 
@@ -38,6 +48,26 @@ public class ElectionSysTestRunner {
         "The following test cases is testing the LISTUTILITIES class. The test class is ListUtilitiesTest.java");
     System.out.println();
     ListUtilitiesTest.main(args);
+
+
+    System.out.println("=================ElectionSys5 Project Test Cases=================");
+    System.out.println();
+
+    System.out.println(
+
+        "All the below test cases are from the test/election.business and test/election.data in the Common5 Project");
+
+
+    DawsonBallotItemTest.main(args);
+    DawsonElectionTest.main(args);
+    DawsonVoterTest.main(args);
+    VoterNameComparatorTest.main(args);
+    VoterPostalCodeComparatorTest.main(args);
+
+    ElectionFileLoaderTest.main(args);
+    ElectionListDBTest.main(args);
+    VoterListDBTest.main(args);
+
 
 
   }
