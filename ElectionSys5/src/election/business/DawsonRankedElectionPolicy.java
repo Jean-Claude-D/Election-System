6a0ff9b0c5101dfa707cc8672d54510437436a73
Gor_F <felicia.gorgatchov@gmail.com>
2017-11-23 16:40:58 -0800

    add private fields and ctor + attempt first to count winner points

---
+++ ElectionSys5/src/election/business/DawsonRankedElectionPolicy.java
@@ -0,0 +1,55 @@
+package election.business;
+
+import java.util.ArrayList;
+import java.util.List;
+import election.business.interfaces.ElectionPolicy;
+
+public class DawsonRankedElectionPolicy implements ElectionPolicy {
+
+  private static final long serialVersionUID = 42031768871L;
+  private DawsonElection election;
+  private List<String> winnerList = new ArrayList<String>();
+
+  public DawsonRankedElectionPolicy(DawsonElection electionParam) {
+    this.election = electionParam;
+  }
+
+  @Override
+  public List<String> getWinner() {
+
+    int[][] results = election.getTally().getVoteBreakdown();
+
+    int winningResult = 0;
+    int points;
+    String contestant;
+
+    for (int choice = 0; choice < results.length; choice++) {
+
+      points = 0;
+
+      for (int ranking = 0; ranking < results[choice].length; ranking++) {
+        if (ranking == 0) {
+          points += results[choice][ranking] * 5;
+        }
+
+        else if (ranking == 1) {
+          points += results[choice][ranking] * 2;
+        }
+
+        else {
+          continue;
+        }
+      }
+
+      if (points > winningResult) {
+        winningResult = points;
+
+      }
+
+    }
+
+    return winnerList;
+
+  }
+
+}
