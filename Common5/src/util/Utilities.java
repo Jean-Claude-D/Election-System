46b9f96c502d04a66095053a701b485ce31079e9
Gor_F <felicia.gorgatchov@gmail.com>
2017-11-26 16:01:34 -0800

    add javadoc comments above each method and constructor

--- Common5/src/util/Utilities.java
+++ Common5/src/util/Utilities.java
@@ -13,10 +13,23 @@ import java.io.ObjectOutputStream;
  */
 public class Utilities {
 
+
+  /**
+   * this empty do-nothing constructor prevents the Utilities class from being instantiated
+   */
   private Utilities() {
 
   }
 
+
+  /**
+   * converts an object into a byte stream so that this byte stream can later be reverted back into
+   * a copy of that object
+   * 
+   * @param object -- an object of any kind
+   * @param fileSpecification -- name of the file where the byte stream will be stored
+   * @throws IOException -- when the object cannot be serialized into the specified file
+   */
   public static void serializeObject(Object object, String fileSpecification) throws IOException {
 
     ObjectOutputStream out = null;
@@ -38,6 +51,17 @@ public class Utilities {
   }// end serialize
 
 
+  /**
+   * reads a byte stream from a given file and converts it back into a copy of the original
+   * serialized object
+   * 
+   * @param fileSpecification -- name of the file in which the bytestream we need is stored
+   * @return object -- a copy of the original object that was previously serialized
+   * @throws IOException -- when the bytestream could not be read from the specified file orthe file
+   *         was not found
+   * @throws ClassNotFoundException -- when the bytestream could not be read from the specified file
+   *         or the file was not found
+   */
   public static Object deserializeObject(String fileSpecification)
       throws IOException, ClassNotFoundException {
 
