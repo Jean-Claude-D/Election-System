dd52cb6b744e2f68e72c6dd3a296e28f7127b157
DesJC <jcd9930@hotmail.ca>
2017-12-11 20:46:03 -0500

    Fix elections.ser, it was corrupted with wrong election because of the bug in setExistingTallyFromSequentialFile

--- ElectionSys5/src/election/ui/gui/StubGUIApp.java
+++ ElectionSys5/src/election/ui/gui/StubGUIApp.java
@@ -6,7 +6,7 @@ import election.business.interfaces.Election;
 import election.business.interfaces.ElectionFactory;
 import election.data.ElectionListDB;
 import election.data.InexistentElectionException;
-import election.data.SequentialTextFileList;
+import election.data.ObjectSerializedList;
 import election.data.VoterListDB;
 import election.data.interfaces.ElectionDAO;
 import election.data.interfaces.VoterDAO;
@@ -14,15 +14,16 @@ import election.data.interfaces.VoterDAO;
 public class StubGUIApp {
   public static void main(String[] args) throws InexistentElectionException {
     ElectionFactory factory = DawsonElectionFactory.DAWSON_ELECTION;
-    VoterDAO voterDb = new VoterListDB(new SequentialTextFileList("datafiles/database/voters.txt",
-        "datafiles/database/elections.txt", "datafiles/database/tally.txt"));
+    VoterDAO voterDb = new VoterListDB(new ObjectSerializedList("datafiles/database/voters.ser",
+        "datafiles/database/elections.ser"));
     ElectionDAO electionDb =
-        new ElectionListDB(new SequentialTextFileList("datafiles/database/voters.txt",
-            "datafiles/database/elections.txt", "datafiles/database/tally.txt"));
+        new ElectionListDB(new ObjectSerializedList("datafiles/database/voters.ser",
+            "datafiles/database/elections.ser"));
     Election e = electionDb.getElection("Brittany independence referendum");
     DawsonElectionOffice model = new DawsonElectionOffice(factory, electionDb, voterDb);
 
     election.ui.gui.MainApp.initContext(model, e);
     javafx.application.Application.launch(election.ui.gui.MainApp.class);
+
   }
 }
